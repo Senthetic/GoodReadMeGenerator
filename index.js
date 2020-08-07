10e811951631ff605c63e80e591c95a8bdcddbf4
Andrew Collier <atc3598@icloud.com>
2020-08-07 15:36:21 -0400

    added the function to write the readme file

--- index.js
+++ index.js
@@ -57,7 +57,13 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) {
+            console.log(err)
+        }
+    });
+}
 
 // function to initialize program
 function init() {}
