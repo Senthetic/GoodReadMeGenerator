bc7c6d77981f915a554b42dcb8ac2d7430456695
Andrew Collier <atc3598@icloud.com>
2020-08-07 15:35:44 -0400

    added questions to ask the user while making readme

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 //links generateMarkdown file
-const generateMarkdown = require("./utils/generateMarkdown.js")
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // added fs const
-const fs = require("fs")
+const fs = require("fs");
 
 // added inquirer const
 const inquirer = require("inquirer");
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is your Project Title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is your project's description?",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "What is the installation for your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is your project going to be used for?",
+  },
+  {
+    type: "checkbox",
+    name: "licenses",
+    message: "What license would you like to use for your project?",
+    choices: ["MIT"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How would you like people to contribute to this project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "Were there any tests done for this project? If so please explain.",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your github username here.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address here.",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
