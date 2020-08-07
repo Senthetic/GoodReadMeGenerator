2785bf39c45211c722b6eb7448205dbc5c3fbbb2
Andrew Collier <atc3598@icloud.com>
2020-08-07 15:34:13 -0400

    added basic setup for homework plus fs and require the generate markdown

---
+++ index.js
@@ -0,0 +1,23 @@
+//links generateMarkdown file
+const generateMarkdown = require("./utils/generateMarkdown.js")
+// added fs const
+const fs = require("fs")
+
+// added inquirer const
+const inquirer = require("inquirer");
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
