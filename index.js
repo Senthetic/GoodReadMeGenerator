0c7ef01876542bdde8b6c8d7ad561e8b4ac4c563
Andrew Collier <atc3598@icloud.com>
2020-08-07 15:43:14 -0400

    final touches

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-//links generateMarkdown file
-const generateMarkdown = require("./utils/generateMarkdown.js");
-// added fs const
-const fs = require("fs");
+//links generate markdown js file to this one to be used
+const generateMarkdown = require("./utils/generateMarkdown.js")
+// add fs const
+const fs = require("fs")
 
-// added inquirer const
+// add inquirer const
 const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is your Project Title?",
-  },
-  {
+
+   {
+       type: "input",
+       name: "title",
+       message: "What is your Project Title?",
+   },
+   {
     type: "input",
     name: "description",
     message: "What is your project's description?",
-  },
-  {
+},
+{
     type: "input",
     name: "install",
     message: "What is the installation for your project?",
-  },
-  {
+},
+{
     type: "input",
     name: "usage",
     message: "What is your project going to be used for?",
-  },
-  {
+},
+{
     type: "checkbox",
     name: "licenses",
     message: "What license would you like to use for your project?",
-    choices: ["MIT"],
-  },
-  {
+    choices: ["MIT"]
+},
+{
     type: "input",
     name: "contributing",
     message: "How would you like people to contribute to this project?",
-  },
-  {
+},
+{
     type: "input",
     name: "tests",
-    message:
-      "Were there any tests done for this project? If so please explain.",
-  },
-  {
+    message: "Were there any tests done for this project? If so please explain.",
+},
+{
     type: "input",
     name: "username",
     message: "Enter your github username here.",
-  },
-  {
+},
+{
     type: "input",
     name: "email",
     message: "Enter your email address here.",
-  },
+},
+  
 ];
-
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile (fileName, data) {
     fs.writeFile(fileName, data, function(err) {
         if (err) {
             console.log(err)
         }
     });
-}
+};
 
 // function to initialize program
-function init() {
+async function init() {
     const response = await inquirer.prompt(questions);
     writeToFile("created-readme.md", generateMarkdown(response));
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,35 +1,31 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown (data) {
+  return `
+  # ${data.title}
+  
   ## Table of Contents
-   -[Description](#description)
-   -[Installation](#install)
-   -[Usage](#usage)
-   -[Licenses](#licenses)
-   -[Contribution](#contribution)
-   -[Tests](#tests)
-   -[Questions](#questions)
-
-   ## Description
-   ### ${data.description}
-   ## Install
-   ### ${data.install}
-   ## Usage
-   ### ${data.usage}
-
-   ## Licenses
-   ![badge](https://img.shields.io/badge/License-${ data.licences }-blue.svg)
-   This project is licensed under ${data.licences}
-
-   ## Contribution
-   ### ${data.contributing}
-   ## Tests 
-   ### ${data.tests}
-   ## Questions
-   ### [${data.username} for github](https://github.com/${data.username})
-   ### ${data.email} for email
+ -[Description](#description)
+ -[Installation](#install)
+ -[Usage](#usage)
+ -[Licenses](#licenses)
+ -[Contribution](#contribution)
+ -[Tests](#tests)
+ -[Questions](#questions)
+ ## Description
+ ### ${data.description}
+ ## Install
+ ### ${data.install}
+ ## Usage
+ ### ${data.usage}
+ ## Licenses
+ ![badge](https://img.shields.io/badge/License-${ data.licences }-blue.svg)
+ This project is licensed under ${data.licences}
+ ## Contribution
+ ### ${data.contributing}
+ ## Tests 
+ ### ${data.tests}
+ ## Questions
+ ### [${data.username} for github](https://github.com/${data.username})
+ ### ${data.email} for email
 `;
 }
-
-module.exports = generateMarkdown;
